f5d4f5b20a8833dea41dd16ad5c8e47ee6a7fb3d
arm <kumaoni01a@gmail.com>
2025-09-06 15:17:45 +0700

    Step 5: Make a firestore service

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// Step 2: Install loading app screen
+import 'package:learn_flutter_67_1_2/first_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
